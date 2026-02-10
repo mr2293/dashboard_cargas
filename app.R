5805f4c915d3142a29c9516296e905a4d23ba0bf
Mateo Rodriguez <mr2293@cornell.edu>
2026-02-10 08:42:46 -0600

    quité a aguirre

--- app.R
+++ app.R
@@ -32,7 +32,7 @@ player_info <- tibble::tibble(
   player = c("Néstor Araujo", "Brian Rodríguez", "Sebastián Cáceres", "Alan Cervantes",
              "Rodolfo Cota", "Erick Sánchez", "Henry Martín", "Israel Reyes",
              "Jonathan Dos Santos", "Kevin Álvarez", "Luis Ángel Malagón", "Miguel Vázquez",
-             "Ramón Juárez", "Alejandro Zendejas", "Rodrigo Aguirre", "Cristian Borja",
+             "Ramón Juárez", "Alejandro Zendejas", "Cristian Borja",
              "Dagoberto Espinoza", "Víctor Dávila", "Santiago Naveda", "Ralph Orquín",
              "Alexis Gutiérrez", "Isaías Violante", "José Raúl Zúñiga", "Patricio Salas",
              "Rodrigo Dourado", "Aaron Mejia", "Raphael Veiga", "Fernando Tapia"
@@ -41,7 +41,7 @@ player_info <- tibble::tibble(
   image = c("nestor.jpg", "brian.jpg", "caceres.jpg", "cervantes.jpg", "cota.jpg",
             "erick_sanchez.jpg", "henry.jpg", "israel_reyes.jpg",
             "jonathan.jpg", "kevin.jpg", "malagon.jpg", "miguel_vazquez.jpg", "ramon.jpg",
-            "zendejas.jpg", "aguirre.jpg", "borja.jpg", "dagoberto.jpg", "davila.jpg",
+            "zendejas.jpg", "borja.jpg", "dagoberto.jpg", "davila.jpg",
             "naveda.jpg", "ralph.jpg", "alexis_gtz.jpg", "violante.jpg",
             "zuniga.jpg", "pato_salas.jpg", "dourado.jpg", "mejia.jpg", "veiga.jpg",
             "tapia.jpg"
@@ -49,14 +49,14 @@ player_info <- tibble::tibble(
             ),
   age = c("29/08/1991", "20/05/2000", "18/08/1999", "17/01/1998", "03/07/1987", "27/09/1999", 
           "18/11/1992", "23/05/2000", "26/04/1990", "15/01/1999", "02/03/1997", "07/02/2004", "09/05/2001", 
-          "07/02/1998", "01/10/1994", "18/02/1993", "17/04/2004", "04/11/1997", "16/04/2001", 
+          "07/02/1998", "18/02/1993", "17/04/2004", "04/11/1997", "16/04/2001", 
           "15/04/2003", "26/02/2000", "20/10/2003", "13/07/1994", "17/02/2004", "17/06/1994", 
           "06/06/2001", "19/06/1995", "17/06/2001"
           # , "11/06/1996"
           ),
   height = c("1.88 m", "1.75 m", "1.80 m", "1.81 m", "1.83 m", "1.67 m",
             "1.77 m", "1.79 m", "1.72 m", "1.76 m",
-             "1.82 m", "1.85 m", "1.82 m", "1.70 m", "1.83 m", "1.79 m", "1.80 m",
+             "1.82 m", "1.85 m", "1.82 m", "1.70 m", "1.79 m", "1.80 m",
              "1.73 m", "1.80 m", "1.77 m", "1.75 m", "1.73 m", "1.80 m",
             "1.85 m", "1.86 m", "1.78 m", "1.76m", "1.85 m"
             # , "1.81 m"
